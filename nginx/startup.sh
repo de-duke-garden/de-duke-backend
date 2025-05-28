6cd98693f72b22564880dc563452454f5e04dade
Ahmad Khidir <khidirahmad05@gmail.com>
2025-05-28 15:00:12 +0100

    changes on nginx

---
+++ nginx/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Paths to the real and dummy certificates
+REAL_CERT="/etc/letsencrypt/live/de-duke.com/fullchain.pem"
+REAL_KEY="/etc/letsencrypt/live/de-duke.com/privkey.pem"
+DUMMY_CERT="/etc/nginx/certs/dummy-cert.pem"
+DUMMY_KEY="/etc/nginx/certs/dummy-key.pem"
+NGINX_CONF="/etc/nginx/conf.d/default.conf"
+
+# Check if the real certificate exists
+if [ -f "$REAL_CERT" ] && [ -f "$REAL_KEY" ]; then
+    echo "Using real SSL certificates."
+    sed -i "s|$DUMMY_CERT|$REAL_CERT|g" $NGINX_CONF
+    sed -i "s|$DUMMY_KEY|$REAL_KEY|g" $NGINX_CONF
+else
+    echo "Using dummy SSL certificates."
+fi
+
+# Start Nginx
+nginx -g "daemon off;"
