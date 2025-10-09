735a2ffb76a79c85dd4dcea857debc477f8d15ce
Ahmad Khidir <khidirahmad05@gmail.com>
2025-10-09 21:08:29 +0100

    v2 refactored

--- backend/main/settings/staging.py
+++ backend/main/settings/staging.py
@@ -25,7 +25,7 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://de-duke.com",
     "http://www.de-duke.com",
-    "http://ec2-35-179-76-99.eu-west-2.compute.amazonaws.com",
+    "http://ec2-13-42-130-253.eu-west-2.compute.amazonaws.com",
 ]
 
 CSRF_COOKIE_SECURE = True
